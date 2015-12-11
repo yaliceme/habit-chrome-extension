d5fd2aeba787cb3a1bfd0ef321ff870289d41ed7
David Stavis <davidstavis@gmail.com>
2015-12-10 20:16:39 -0800

    Loading initial data from localstorage sync

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,6 +1,19 @@
-var habit = document.getElementById("habit").value;
-var consecutiveDays = document.getElementById("consecutiveDays").value;
-var lastCheckin = document.getElementById("lastCheckin").value;
+var data = {}
+var habit = ''
+var consecutiveDays = ''
+var lastCheckin = ''
+
+chrome.storage.sync.get(null, function(response){
+  data = response
+  habit = data.habit;
+  consecutiveDays = data.consecutiveDays;
+  lastCheckin = data.lastCheckin;
+});
 
 
-chrome.storage.sync.set({'habit': habit, 'consecutiveDays': consecutiveDays, 'lastCheckin': lastCheckin});
+// chrome.storage.sync.set({'habit': habit, 'consecutiveDays': consecutiveDays, 'lastCheckin': lastCheckin});
+
+
+// var habit = document.getElementById("habit").value;
+// var consecutiveDays = document.getElementById("consecutiveDays").value;
+// var lastCheckin = document.getElementById("lastCheckin").value;
