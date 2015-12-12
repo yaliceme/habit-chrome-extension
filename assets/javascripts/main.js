6aa411bfbf5adc8dbe7c80618edea580aa9b6d6d
yaliceme <aliceyu@Alices-Air-2.lan>
2015-12-11 19:35:25 -0800

    Add event listeners and handlers for all inputs

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,38 +10,29 @@ chrome.storage.sync.get(null, function(response){
   consecutiveDays = data.consecutiveDays;
   lastCheckin = data.lastCheckin;
 
-  // Set the value of the inputs to the data from storage
-
   //TODO add if statements to set the value to some default value if habit etc do not have a value
   document.getElementById("habit").value = habit;
   document.getElementById("consecutiveDays").value = consecutiveDays;
   document.getElementById("lastCheckin").value = lastCheckin;
 });
 
-// set event listeners
-// When the "value" property of habit changes
+// event handlers for input changes
+var saveHabit = function(event){
+  console.log(event);
+  chrome.storage.sync.set({"habit": event.target.value});
+};
 
-document.getElementById("habit").addEventListener("change", function () {
-  alert("you made a change");
-  //chrome.storage.sync.set call to modify 'habit' property to document.getElementById("habit").value
-});
+var saveConsecutiveDays = function(event){
+  console.log(event);
+  chrome.storage.sync.set({"consecutiveDays": event.target.value});
+};
 
+var saveLastCheckin = function(event){
+  console.log(event);
+  chrome.storage.sync.set({"lastCheckin": event.target.value});
+};
 
-document.getElementById("consecutiveDays").value = consecutiveDays;
-document.getElementById("lastCheckin").value = lastCheckin;
-
-// Event handler for habit changes
-var saveHabit = function(response){
-  alert(response);
-}
-
-var updateAndSaveData = function(){
-  // Update the values of our data models to the new value
-  habit = document.getElementById("habit").value;
-  consecutiveDays = document.getElementById("consecutiveDays").value;
-  lastCheckin = document.getElementById("lastCheckin").value;
-
-
-  // Save the data to storage
-  chrome.storage.sync.set({'habit': habit, 'consecutiveDays': consecutiveDays, 'lastCheckin': lastCheckin});
-}
+// event listeners for when the "value" properties of the form inputs change
+document.getElementById("habit").addEventListener("change", saveHabit); 
+document.getElementById("consecutiveDays").addEventListener("change", saveConsecutiveDays); 
+document.getElementById("lastCheckin").addEventListener("change", saveLastCheckin);
